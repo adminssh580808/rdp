46bfe09169d3bc9c01570e212057f5532e11e871
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2019-03-11 22:44:40 +0700

    Delete tightvnc.sh

--- ubuntu/tightvnc.sh
+++
@@ -1,6 +0,0 @@
-echo "===================================="
-echo "        Selamat Datang Admin"
-echo "===================================="
-apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
-apt-get install -y tightvncserver
-tightvncserver :1
