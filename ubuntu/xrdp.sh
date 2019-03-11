3e5f5943f8c7c98b6b3e0ba8c7ea2a88baa08314
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2019-03-11 22:47:58 +0700

    Update xrdp.sh

--- ubuntu/xrdp.sh
+++ ubuntu/xrdp.sh
@@ -10,4 +10,3 @@ sudo service xrdp start
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
 tightvncserver :1
-reboot
