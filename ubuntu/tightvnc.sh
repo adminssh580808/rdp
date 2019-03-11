71c5c8eae8a57b0c70c58507d5c9550cba457bc4
adminssh580808 <47466957+adminssh580808@users.noreply.github.com>
2019-03-11 22:29:05 +0700

    Update tightvnc.sh

--- ubuntu/tightvnc.sh
+++ ubuntu/tightvnc.sh
@@ -1,5 +1,5 @@
 echo "===================================="
-echo "Selamat Datang di Fawzya.Net"
+echo "      Selamat Datang di Admin"
 echo "===================================="
 apt-get install -y xfonts-100dpi xfonts-100dpi-transcoded xfonts-75dpi xfonts-75dpi-transcoded xfonts-base
 apt-get install -y tightvncserver
